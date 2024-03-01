3421fb4196cfce279b4697876b16a2f87b3e1486
KRB001 <kieranbentley0@gmail.com>
2024-03-01 12:39:06 -0500

    framework for cli class

---
+++ src/CASHTRACK.java
@@ -0,0 +1,7 @@
+public class CASHTRACK {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to CA$HTRACK...");
+    }
+
+}
--- src/CommandLineInterface.java
+++ src/CommandLineInterface.java
@@ -1,7 +1,72 @@
+/**
+ * CLI for CA$HTRACK
+ * @author Kieran Bentley
+ * @version 1.0
+ */
 public class CommandLineInterface {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to CA$HTRACK...");
+    private Account account;
+
+    public CommandLineInterface(){
+
+
+
+    }
+
+    /**
+     * Enter dialogue for budgeted purchase tracking
+     */
+    public void track() {
+
+
+
+    }
+
+    /**
+     * Enter dialogue for sandbox purchase tracking
+     */
+    public void trackSandbox() {
+
+
+
+    }
+
+    /**
+     * Enter dialogue for updating account info and settings
+     */
+    public void updateAccount() {
+
+
+
+    }
+
+    /**
+     * Enter dialogue for getting analytics information
+     */
+    public void getAnalytics() {
+
+
+
+    }
+
+    /**
+     * Get an advice printout
+     */
+    public void getAdvice() {
+
+
+
+    }
+
+    /**
+     * Internal class for validating amounts
+     * @param amount the amount to be validated
+     * @return is the amount valid
+     */
+    private boolean isAmountValid(double amount) {
+
+        return false;
+
     }
 
 }
