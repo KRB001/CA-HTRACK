464f0b17f7a4b85378c444b3ba62332449ff21f3
KRB001 <kieranbentley0@gmail.com>
2024-03-06 10:41:14 -0500

    done for now

--- src/main/CommandLineInterface.java
+++ src/main/CommandLineInterface.java
@@ -80,10 +80,58 @@ public class CommandLineInterface {
     /**
      * Enter dialogue for budgeted purchase tracking
      */
-    private static void track(BufferedReader reader, Account account) {
+    private static void track(BufferedReader reader, Account account) throws IOException {
 
+        for (int ndx = 0; ndx < 20; ndx++) {
+            System.out.println("\n");
+        }
 
+        System.out.println("You are now tracking finances with budget.\n");
+        System.out.println("\tEnter a valid numerical value to log a purchase (add the argument -p to make it a payment instead)");
+        System.out.println("\tEnter [inf] to get information about your account");
+        System.out.println("\tEnter [esc] to exit to the main interface");
 
+        String interfaceChoice = "";
+
+        while (!interfaceChoice.equals("esc")) {
+
+            boolean paymentFlag = false;
+            interfaceChoice = reader.readLine();
+
+            if (interfaceChoice.contains("-p")) {
+                interfaceChoice = interfaceChoice.replace("-p", "").strip();
+                paymentFlag = true;
+            }
+
+            try {
+                double amount = Double.parseDouble(interfaceChoice);
+                if (paymentFlag) {
+                    account.addPayment(amount);
+                    System.out.print("Successfully logged a $" + amount + " payment.");
+                } else {
+                    if (account.purchase(amount)) {
+                        System.out.print("Successfully logged a $" + amount + " purchase.");
+                    } else {
+                        System.out.print("Insufficient funds");
+                    }
+                }
+            } catch (NumberFormatException e) {
+                if (interfaceChoice.equals("inf")) {
+                    System.out.println("\nACCOUNT INFORMATION" +
+                            "\n\t Created on: " + account.getStartDate() +
+                            "\n\t Current balance: " + account.getBalance() +
+                            "\n\t Weekly budget: " + account.getAllowance() +
+                            "\n\t Is rollover allowed? " + account.isRolloverAllowed() +
+                            "\n");
+                } else if (interfaceChoice.equals("esc")) {
+                    System.out.println("Exiting budget tracker...");
+                } else {
+                    System.out.println("'" + interfaceChoice + "' is not a valid command");
+                }
+
+            }
+
+        }
     }
 
     /**
